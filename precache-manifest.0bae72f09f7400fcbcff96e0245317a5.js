f01a007f885a3ddc0f51d98499020ac35d7de718
Nguyễn Nguyễn <nttn9x@gmail.com>
2020-08-19 18:35:21 +0700

    Updates

---
+++ precache-manifest.0bae72f09f7400fcbcff96e0245317a5.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685a9d6d7ae495e33d1c2fb584450fdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5da4f2215c8f58fc4127",
+    "url": "/static/css/10.007ed5b1.chunk.css"
+  },
+  {
+    "revision": "721a1086f9d94e3b0df6",
+    "url": "/static/css/4.2e2259cb.chunk.css"
+  },
+  {
+    "revision": "9b0db5fe6b5b2a4a6172",
+    "url": "/static/css/8.bf0413c3.chunk.css"
+  },
+  {
+    "revision": "03d540f4290ca9d67e39",
+    "url": "/static/css/9.a28c088f.chunk.css"
+  },
+  {
+    "revision": "f07e1ca0dabc702b385a",
+    "url": "/static/css/main.e7a89ede.chunk.css"
+  },
+  {
+    "revision": "d71ccdb86c22bb1ea0d4",
+    "url": "/static/js/0.f3e33d5a.chunk.js"
+  },
+  {
+    "revision": "ddd0d8a0d8e322723363",
+    "url": "/static/js/1.b09a6797.chunk.js"
+  },
+  {
+    "revision": "5da4f2215c8f58fc4127",
+    "url": "/static/js/10.8ba3abb8.chunk.js"
+  },
+  {
+    "revision": "119f5cd4939117586712",
+    "url": "/static/js/11.744ea110.chunk.js"
+  },
+  {
+    "revision": "ef886ba1b0e47fc3d454",
+    "url": "/static/js/12.52e51be6.chunk.js"
+  },
+  {
+    "revision": "8f6143164cb66048ed14",
+    "url": "/static/js/13.493aa50b.chunk.js"
+  },
+  {
+    "revision": "4150009efed8292c358f",
+    "url": "/static/js/2.555b54d9.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/static/js/2.555b54d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c53c22dfe0942bdea95",
+    "url": "/static/js/3.0d28908d.chunk.js"
+  },
+  {
+    "revision": "ee613d889184d1b59f55a863249b188c",
+    "url": "/static/js/3.0d28908d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "721a1086f9d94e3b0df6",
+    "url": "/static/js/4.381678a0.chunk.js"
+  },
+  {
+    "revision": "9180c5c666aa128e0144",
+    "url": "/static/js/5.5ddee0e2.chunk.js"
+  },
+  {
+    "revision": "9b0db5fe6b5b2a4a6172",
+    "url": "/static/js/8.923f349b.chunk.js"
+  },
+  {
+    "revision": "a41c879f8bdb54796b2468bd148c705b",
+    "url": "/static/js/8.923f349b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03d540f4290ca9d67e39",
+    "url": "/static/js/9.5b0dd548.chunk.js"
+  },
+  {
+    "revision": "f07e1ca0dabc702b385a",
+    "url": "/static/js/main.4b9b05f9.chunk.js"
+  },
+  {
+    "revision": "72cc952b2cb3cff2f372",
+    "url": "/static/js/runtime-main.ddd5e8dd.js"
+  },
+  {
+    "revision": "b36db5af25b0da73de30fa26ff198073",
+    "url": "/static/media/app-builder-bg.b36db5af.svg"
+  },
+  {
+    "revision": "86a86274ea8a5c3618d4d5317cbc9e00",
+    "url": "/static/media/banner.86a86274.png"
+  },
+  {
+    "revision": "e34c4a907451741620b17e09b9c7f0a0",
+    "url": "/static/media/icon-add.e34c4a90.png"
+  },
+  {
+    "revision": "169d2e4222ce50b8cd54a38e174201a1",
+    "url": "/static/media/photo.169d2e42.png"
+  },
+  {
+    "revision": "9c7ba108128f8afcea59754a3790f9cd",
+    "url": "/static/media/portrait.9c7ba108.png"
+  },
+  {
+    "revision": "d586706c27e37b866653635ab8c968fb",
+    "url": "/static/media/signin-image.d586706c.jpg"
+  },
+  {
+    "revision": "75eb7c4e5d58a0a3f0d387b9bc44e080",
+    "url": "/static/media/signup-image.75eb7c4e.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "4536dd740e5c0e96a38dfc474526f196",
+    "url": "/static/media/woo-commerce.4536dd74.png"
+  },
+  {
+    "revision": "accafba0e22a4f33f948811221f09537",
+    "url": "/static/media/wordPress_blue_logo.accafba0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/builder-frontend/precache-manifest.f03fc6dfb476b621ea2a83870550d9f3.js"
+  "/precache-manifest.0bae72f09f7400fcbcff96e0245317a5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/builder-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
